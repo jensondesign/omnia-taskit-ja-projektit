2696a9ea5e0936653d7ba08ce1dcf56302391515
Jens <bgk146@myy.haaga-helia.fi>
2023-09-11 18:20:39 +0300

    Infinite scroll

---
+++ HTML-ja-CSS/infinite-scroll-php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Infinite Scrolling PHP</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+
+<body>
+    <div id="post-container">
+        <!-- Tähän lisätään uudet postaukset jatkuvassa scrollauksessa -->
+    </div>
+    <div id="loading" style="display: none;">Loading...</div>
+    <script src="script.js"></script>
+</body>
+
+</html>
