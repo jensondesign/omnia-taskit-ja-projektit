199e163b1417be33d2f476e99b3a02706f457cef
Jens <bgk146@myy.haaga-helia.fi>
2023-09-19 14:21:17 +0300

    html > php ja käsittelijä

--- Nettikaupan-rekisteröitymislomake/rekisteroidy.php
+++ Nettikaupan-rekisteröitymislomake/rekisteroidy.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $etunimi = $_POST["firstName"];
     $sukunimi = $_POST["lastName"];
     $kayttajanimi = $_POST["username"];
-    $salasana = $_POST["password"];
+    $salasana = password_hash($_POST["password"], PASSWORD_DEFAULT); // Salaa salasanan
     $sahkoposti = $_POST["email"];
     $osoite = $_POST["address"];
     $asunnonnro = $_POST["address2"];
@@ -16,27 +16,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $maksutapa = $_POST["maksutapa"];
     $palaute = $_POST["palaute"];
 
-    // Yhdistä tietokantaan
-    $servername = "datasql2.westeurope.cloudapp.azure.com:8081";
-    $username = "millerje";
-    $password = "Yz!u,zpz^S4G%RZ";
+    // Yhdistä tietokantaan oikein
+    $servername = "datasql2.westeurope.cloudapp.azure.com:8081"; // Korvaa oikealla tietokantapalvelimen osoitteella
+    $username = "millerje"; // Korvaa oikealla käyttäjänimellä
+    $password = "Yz!u,zpz^S4G%RZ"; // Korvaa oikealla salasanalla
     $dbname = "neilikka";
+
+    // Luo yhteys
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
     // Tarkista yhteys
     if ($conn->connect_error) {
         die("Yhteys epäonnistui: " . $conn->connect_error);
     }
 
-    // Valmistele SQL-lauseke ja suorita se tietokantaan
-    $sql = "INSERT INTO users (etunimi, sukunimi, kayttajanimi, salasana, sahkoposti, osoite, asunnonnro, maa, maakunta, postinumero, maksutapa, palaute)
-    VALUES ('$etunimi', '$sukunimi', '$kayttajanimi', '$salasana', '$sahkoposti', '$osoite', '$asunnonnro', '$maa', '$maakunta', '$postinumero', '$maksutapa', '$palaute')";
+    // Valmistele SQL-lauseke
+    $stmt = $conn->prepare("INSERT INTO users (etunimi, sukunimi, kayttajanimi, salasana, sahkoposti, osoite, asunnonnro, maa, maakunta, postinumero, maksutapa, palaute) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssssssss", $etunimi, $sukunimi, $kayttajanimi, $salasana, $sahkoposti, $osoite, $asunnonnro, $maa, $maakunta, $postinumero, $maksutapa, $palaute);
 
-    if ($conn->query($sql) === TRUE) {
+    if ($stmt->execute()) {
         echo "Rekisteröityminen onnistui!";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $stmt->error;
     }
 
     // Sulje yhteys
+    $stmt->close();
     $conn->close();
 }
