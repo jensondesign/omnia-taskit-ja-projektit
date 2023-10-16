c95a08eb0ca21de104114e1c9b2f8b3d7f30f998
Jens <bgk146@myy.haaga-helia.fi>
2023-10-16 13:43:08 +0300

    neilikka-2.0

--- neilikka-2.0/scripts.js
+++ neilikka-2.0/scripts.js
@@ -1,33 +1,11 @@
-// Hae tarvittavat elementit
-const hamppisMenu = document.getElementById("hamppis-menu");
-const overlayMenu = document.getElementById("overlay");
-const closeButton = document.getElementById("sulkunappi");
-const iconElement = hamppisMenu.querySelector("i");
+const menuIcon = document.querySelector(".nav-hamburger");
+const navOverlay = document.querySelector(".nav-overlay");
+const closeBtn = document.querySelector(".nav-close");
 
-// Aseta sulkuikonille FontAwesome-ikoni ja luokat
-closeButton.className = "fa-solid fa-xmark sulkunappi";
-closeButton.style.display = "none"; // Piilota sulkuikoni aluksi
-
-// Lisää sulkuikoni valikon yläkulmaan
-overlayMenu.appendChild(closeButton);
-
-// Kuuntele hamppis-ikonin klikkausta
-hamppisMenu.addEventListener("click", () => {
-  overlayMenu.classList.toggle("active"); // Näytä/Piilota valikko
-  if (iconElement.classList.contains("fa-bars")) {
-    iconElement.classList.remove("fa-bars");
-    iconElement.classList.add("fa-xmark");
-  } else {
-    iconElement.classList.remove("fa-xmark");
-    iconElement.classList.add("fa-bars");
-  }
-
-  document.body.classList.toggle("overlay-active"); // Menun tausta
+menuIcon.addEventListener("click", () => {
+  navOverlay.classList.toggle("show");
 });
 
-// Kuuntele sulkuikonin klikkausta
-closeButton.addEventListener("click", () => {
-  overlayMenu.classList.remove("active"); // Piilota valikko
-  iconElement.classList.remove("fa-xmark");
-  iconElement.classList.add("fa-bars");
+closeBtn.addEventListener("click", () => {
+  navOverlay.classList.remove("show");
 });
